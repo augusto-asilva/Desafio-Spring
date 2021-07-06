b2e085e70a76f82d4f2f272022a8ff0b1fc849f9
“wesleyMeli” <wesley.ferreira@mercadolivre.com>
2021-07-06 17:45:29 -0300

    :arrow_up: Adicao e configuracao da Dependencia H2. feat:Adicao do arquivo data.sql com criacao de tabelas e insercao de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,58 @@
+DROP TABLE IF EXISTS USERS;
+DROP TABLE IF EXISTS FOLLOWERS;
+DROP TABLE IF EXISTS PRODUCTS;
+DROP TABLE IF EXISTS POSTS;
+
+CREATE TABLE USERS (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  name VARCHAR(250)
+);
+
+INSERT INTO USERS (id, name) VALUES
+  (0, 'Bill Gates'),
+  (1, 'Mark Zuckerberg'),
+  (2, 'Angelina Jolie');
+  
+CREATE TABLE FOLLOWERS (
+  user_id INT,
+  following_id INT,
+  FOREIGN KEY(user_id) REFERENCES USERS(id),
+  FOREIGN KEY(following_id) REFERENCES USERS(id)
+);
+
+INSERT INTO FOLLOWERS (user_id, following_id) VALUES
+  (1, 0),
+  (1, 2),
+  (0, 2),
+  (2, 0);
+  
+CREATE TABLE PRODUCTS (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  name VARCHAR(250),
+  type VARCHAR(250),
+  brand VARCHAR(250),
+  color VARCHAR(250),
+  notes VARCHAR(250)
+);
+
+INSERT INTO PRODUCTS (id, name, type, brand, color, notes) VALUES
+  (0, 'Bicicleta', 'Corrida', 'Caloi', 'Vermelha', 'Bicicleta para Corrida'),
+  (1, 'Headset RGB Inalámbrico', 'Gamer', 'Razer','Green with RGB', 'Sin Bateria'),
+  (2, 'Cadeira Gamer', 'Gamer', 'Racer', 'Red & Black', 'Special Edition'),
+  (3, 'MacBook Pro', 'Computing', 'Apple', 'Silver', 'touch screen');
+  
+  
+CREATE TABLE POSTS (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  date date,
+  product_id INT,
+  user_id INT,
+  FOREIGN KEY(user_id) REFERENCES USERS(id),
+  FOREIGN KEY(product_id) REFERENCES PRODUCTS(id)
+);
+
+
+INSERT INTO POSTS (id, date, product_id, user_id) VALUES
+  (0, '2021-07-05', 0, 1),
+  (1, '2021-07-06', 1, 0);
+    
