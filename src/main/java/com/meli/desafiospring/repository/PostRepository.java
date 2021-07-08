6ad8f1c629878e9ac6c8360c1229c93a02927ed2
iBianquini <iago.bianquini@mercadolivre.com>
2021-07-08 11:42:26 -0300

    Update src/main/java/com/meli/desafiospring/repository/PostRepository.java
    Co-authored-by: Daniel Silva Nunes <daniel.nunes@mercadolivre.com>

--- src/main/java/com/meli/desafiospring/repository/PostRepository.java
+++ src/main/java/com/meli/desafiospring/repository/PostRepository.java
@@ -4,7 +4,6 @@ import com.meli.desafiospring.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.time.LocalDate;
 import java.util.List;
 
