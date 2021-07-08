115f333dc8b96207dff4c838e232e8a8df522982
iBianquini <iago.bianquini@mercadolivre.com>
2021-07-08 11:42:12 -0300

    Update src/main/java/com/meli/desafiospring/service/PostService.java
    Co-authored-by: Daniel Silva Nunes <daniel.nunes@mercadolivre.com>

--- src/main/java/com/meli/desafiospring/service/PostService.java
+++ src/main/java/com/meli/desafiospring/service/PostService.java
@@ -8,7 +8,6 @@ import com.meli.desafiospring.repository.PostRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
