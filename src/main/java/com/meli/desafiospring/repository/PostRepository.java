241814c044a72e8beda2fe67b91638a4ee9a8984
dsnunes07 <daniel.nunes@mercadolivre.com>
2021-07-08 16:41:03 -0300

    feat: US0012 finalizada

--- src/main/java/com/meli/desafiospring/repository/PostRepository.java
+++ src/main/java/com/meli/desafiospring/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.meli.desafiospring.repository;
 
 import com.meli.desafiospring.domain.Post;
+import com.meli.desafiospring.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -14,4 +15,6 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
 
     @Query(value = "SELECT * FROM POSTS WHERE USER_ID IN (:usersId) AND DATE > :currentDate", nativeQuery = true)
     List<Post> getPosts(List<Integer> usersId, LocalDate currentDate);
+
+    List<Post> findPostsByUserAndHasPromoIsTrue(User user);
 }
